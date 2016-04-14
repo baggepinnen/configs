b9169feb3203c292158522c1d612f8c607a7a035
baggepinnen <baggepinnen@gmail.com>
2016-04-14 16:28:49 +0200

    Update setup.sh

--- scripts/work/setup.sh
+++ scripts/work/setup.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 
+
 wget -o "atom.x86_64.rpm" "https://atom.io/download/rpm"
 sudo dnf install atom.x86_64.rpm
 
 sudo pip install jupyter
+
+
+apm install indent-detective
+apm install ink
+apm install julia-client
+apm install language-julia
+apm install latex-completions
+
+apm install hydrogen
+apm install auto-indent
+apm install highlight-selected
+apm install language-latex
+apm install latex
+apm install minimap
+apm install pdf-view
+apm install jude
+
+apm install duotone-dark-earth-syntax
+apm install rusty-dark-syntax
