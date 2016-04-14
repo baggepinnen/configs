a032c266e82a33ad152973b024ba009dc05b0e63
baggepinnen <baggepinnen@gmail.com>
2016-04-14 16:21:43 +0200

    Add install commands for atom/julia packages

--- scripts/work/setup.sh
+++ scripts/work/setup.sh
@@ -4,3 +4,11 @@ wget -o "atom.x86_64.rpm" "https://atom.io/download/rpm"
 sudo dnf install atom.x86_64.rpm
 
 sudo pip install jupyter
+
+apm install indent-detective
+apm install ink
+apm install julia-client
+apm install language-julia
+apm install latex-completions
+
+
