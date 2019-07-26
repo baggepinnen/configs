1d8a33296594de309dc5b6542d77ab98dae45070
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-07-26 09:25:27 +0800

    Update install_pygments_julialexer.bash

--- scripts/install_pygments_julialexer.bash
+++ scripts/install_pygments_julialexer.bash
@@ -2,3 +2,4 @@ git clone https://github.com/sisl/pygments-julia
 cd pygments-julia/
 sudo python setup.py install
 pygmentize -L lexers | grep julia1
+wget https://github.com/baggepinnen/configs/blob/master/scripts/atomone.py
