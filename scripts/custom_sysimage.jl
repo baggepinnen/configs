26ebb299dd076f78237d8ad20fd2ae19358839a3
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2021-10-19 08:17:56 +0200

    updates

--- scripts/custom_sysimage.jl
+++ scripts/custom_sysimage.jl
@@ -1,22 +1,18 @@
 #error("Use julia formatter a bit so Juno will be quicker at formatting")
-# using Pkg
-# Pkg.update()
-# using Random
-# Random.__init__()
+
 import GR
-# GR.__init__()
 using Plots
 gr()
 plot(randn(3)) |> display
 plot(randn(3), randn(3)) |> display
 heatmap(randn(5, 5)) |> display
 
-# import Plotly
-plotly()
-plot(randn(3)) |> display
-plot(randn(3), randn(3)) |> display
-f = heatmap(randn(5, 5))
-display(f)
+import Plotly
+plotly(show = false)
+f1 = plot(randn(3))
+f2 = plot(randn(3), randn(3))
+f3 = heatmap(randn(5, 5))
+display(plot(f1, f2, f3))
 
 
 
@@ -34,31 +30,45 @@ a .* a
 sum(a)
 
 
-using Optim, DSP, ForwardDiff, ComponentArrays
+using Optim, DSP, ForwardDiff, JSON3, TotalLeastSquares
+using DynamicAxisWarping
+dtw(randn(100), randn(101))
+dtw(randn(Float32, 100), randn(Float32, 101))
 using StatsBase
-using Turing
+# using Turing
 using OrdinaryDiffEq
 
 using JuliaFormatter
 JuliaFormatter.format(@__FILE__)
 
 
+using Pluto, PlutoUI
+plutotask = @async Pluto.run(launch_browser = false)
+sleep(5)
+
+
+
 using PackageCompiler
 @time PackageCompiler.create_sysimage(
     [
-        :StaticArrays,
         :BenchmarkTools,
-        :StatsBase,
-        :Turing,
-        :Optim,
+        # :ComponentArrays,
         :DSP,
+        :DynamicAxisWarping,
         :ForwardDiff,
-        :ComponentArrays,
-        :OrdinaryDiffEq,
-        # :Plotly,
-        :Plots,
         :GR,
+        :JSON3,
         :JuliaFormatter,
+        :Optim,
+        :OrdinaryDiffEq,
+        :Plots,
+        :Pluto,
+        :PlutoUI,
+        :StaticArrays,
+        :StatsBase,
+        :TotalLeastSquares,
+        :Plotly,
+        # :Turing,
     ];
     precompile_statements_file = "precompile.jl",
     # precompile_execution_file = "/home/fredrikb/configs/scripts/custom_sysimage.jl",
@@ -66,10 +76,3 @@ using PackageCompiler
     sysimage_path = "sys_$(VERSION).so",
 )
 exit()
-
-
-# empty!(LOAD_PATH)
-# empty!(DEPOT_PATH)
-
-#julia --output-o sys.o -J"/home/fredrikb/julia/usr/lib/julia/sys.so" custom_sysimage.jl
-# gcc -shared -o sys.so -Wl,--whole-archive sys.o -Wl,--no-whole-archive -L"/home/fredrikb/julia/usr/lib" -ljulia
